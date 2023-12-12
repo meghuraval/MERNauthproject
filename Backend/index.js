b714a5b83c56fc3dc0f0147100a06f086bd0738a
meghu <meghuraval1234@yahoo.com>
2023-12-11 21:25:18 -0500

    added connection to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+try {
+  mongoose.connect(process.env.MONGO);
+} catch (error) {
+  console.log(error);
+}
 
 const app = express();
 
-const port = 3000;
+app.use(morgan("dev"));
 
-app.listen(3000, () => {
-  console.log("listening on port: " + port);
+app.get("/", (req, res) => res.send("hello the method is: " + req.method));
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port: " + process.env.PORT);
 });
