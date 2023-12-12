d30d096e0a19fd6f85d816d4466fe0fd59f48436
meghu <meghuraval1234@yahoo.com>
2023-12-11 19:31:10 -0500

    added routes and created pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Sign-up";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
