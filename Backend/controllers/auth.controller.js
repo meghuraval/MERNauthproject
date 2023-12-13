4c4c664a253f0528781f09f2b73afc6c768cec19
meghu <meghuraval1234@yahoo.com>
2023-12-13 12:02:06 -0500

    create signin api route

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,10 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
+const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const signup = async (req, res, next) => {
   //the below 4 lines create the newuser object based on teh inputs received from the front end without any validations
@@ -16,4 +21,27 @@ const signup = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = signup;
+
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) return next(errorHandler(404, "User Not Found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+    //the id value in mongodb is writte as _id, its good to use _id to check and give token, becuase the id is very unique and hard to find which is more secure rather than email and password.
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...restOfElements } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(restOfElements);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  signup,
+  signin,
+};
