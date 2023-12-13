b7f8e606d612636c0a39029557bfb82650d70f6d
meghu <meghuraval1234@yahoo.com>
2023-12-12 23:13:49 -0500

    extra adds that didnt add the previous time

--- Backend/Routes/auth.route.js
+++ Backend/Routes/auth.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const signup = require("../controllers/auth.controller");
 
+//Defines the route using Express Router for handling the POST request to /signup.
+//Assigns the request to the corresponding controller function (signup) responsible for handling the creation of the user.
 const authrouter = express.Router();
 
 authrouter.post("/signup", signup);
--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
-const errorHandler = require("../utils/error");
 
 const signup = async (req, res, next) => {
+  //the below 4 lines create the newuser object based on teh inputs received from the front end without any validations
   console.log(req.body);
   const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = await bcryptjs.hash(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
+  //these below 5 lines will validate the created newuser object to see wheether the values received match with the user schema before saving the newuser to teh database, if any errors occur it will send an error back or else it will accept the newuser object and add it to the database.
   try {
     await newUser.save();
     res.status(200).json({ message: "user created" });
